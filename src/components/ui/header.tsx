b1b5872ec4ff56391d9da9a50785f61f69d6f283
Conte0745 <r92720473@gmail.com>
2025-07-21 01:57:15 +0900

    increment v

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -21,7 +21,7 @@ export function Header() {
       gap={4}
     >
       <Heading size="md">
-        <Link href="/">ToDoアプリ v1.0.9</Link>
+        <Link href="/">ToDoアプリ v1.0.10</Link>
       </Heading>
       <Spacer />
       <Box>
